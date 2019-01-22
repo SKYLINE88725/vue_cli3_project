a1f57862ab6d4bb31cd3760e60e766fd88957e3e
SKYLINE88725 <skyline88725@outlook.com>
2019-01-22 09:19:41 +0800

    2019年1月22日09:19:19提交

---
+++ src/components/top_detail.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="top_detail" >
+    <a href="/home" class="to-home">
+      <i class="iconfont icon-shouye" ></i>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'top_detail',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  #top_detail{
+    width:100%;
+    box-sizing: border-box;
+    padding:0 .2rem;
+    height:.6rem;
+    line-height: .6rem;
+    font-size: .4rem;
+    color:#ff0;
+    position: fixed;
+    left:0;
+    right:0;
+    top:0;
+    z-index: 998;
+    // background:#5bd8ff;
+    background: linear-gradient(90deg, #5bd8ff 33.33%,#9578ff 66.66%, #ff0000 100%);
+    .to-home{
+      float:right;
+      display: block;
+      height: .6rem;
+      width: .4rem;
+      color:$theme-color;
+      font:.4rem/.6rem '';
+      .iconfont{
+        font:.4rem/.6rem '';
+      }
+    }
+   
+  } 
+</style>
--- src/views/detail.vue
+++ src/views/detail.vue
@@ -1,19 +1,19 @@
 <template>
-  <div id="detail">
-    <!-- <top :bt="bt" :dataName="bt"></top> -->
+  <div id="detail" ref="detail">
+    <top ref='top'></top>
     <h3></h3>
-    <iframe :src="newItem" id="Iframe" frameborder="0" scrolling="no" style="border:0px;"></iframe>
+    <iframe :src="newItem" ref="iframe" id="iframe"  frameborder="0"  style="border:0px;"></iframe>
   </div>
 </template>
 
 <script>
-import top from "../components/top.vue";
+import top from "../components/top_detail.vue";
 import {mapGetters,mapActions} from 'vuex';
 import * as types from '../store/types';
 export default {
   name: 'detail',
-  // props:{bt,newUrl,dataName},
   components:{top},
+  
   data(){
     return{
       list:[],
@@ -21,6 +21,10 @@ export default {
     }
   },
   mounted(){
+    this.$refs.iframe.width =  this.$refs.detail.offsetWidth;
+    let topHeight = window.innerHeight;
+    this.$refs.iframe.height = topHeight;
+    console.log(this.$refs.top.offsetTop)
   },
   computed:mapGetters([
     'bFoot','bLoading','newItem'
@@ -30,5 +34,19 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
+  #detail{
+    margin-left: 0px;
+    margin-top: 0px;
+    margin-right: 0px;
+    margin-bottom: 0px;
+    overflow: hidden;
+    #iframe{
+     position: absolute;
+     top:.6rem;
+     left:0;
+     right:0;
+     bottom: 0;
+    }
+  }
 </style>
 
